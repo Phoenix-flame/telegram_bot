dce8d1e5d3a69b03db7eed151cb7fd40844de832
Phoenix-flame <alireza17010@gmail.com>
2023-01-31 12:47:14 +0330

    new libraries

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,55 @@
 
-use telebot::Bot;
+use futures::Future;
+use select::predicate::Predicate;
+use teloxide::prelude::*;
 use std::env;
-use futures::stream::Stream;
 use reqwest::Client;
 use select::document::Document;
 use select::predicate::Name;
-use telebot::functions::*;
-fn main() {
-    // Create the bot
-    let mut bot = Bot::new(&env::var("6147407906:AAEp1nNMP2Y5YaRIrNAsVVa0fnZsL7-eYV4").unwrap()).update_interval(200);
+use select::predicate::Class;
+use futures::stream::Stream;
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let bot = Bot::from_env();
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
+    // let mut bot = Bot::new("6147407906:AAEp1nNMP2Y5YaRIrNAsVVa0fnZsL7-eYV4").update_interval(200);
+
+    // let handle = bot.new_cmd("/news")
+    //     .and_then(|(bot, msg)| {
+    //         println!("{:?}", msg);
+    //         bot.message(msg.chat.id, msg.text).send()
+    //     })
+    //     .for_each(|_| Ok(()));
+
+
+    //     // .and_then(|(bot, msg)| {
+    //     //     let news = get_latest_news().await.unwrap();
  
-    // Register a reply command which answers a message
-    let handle = bot.new_cmd("/reply")
-        .and_then(|(bot, msg)| {
-            let mut text = msg.text.unwrap().clone();
-            if text.is_empty() {
-                text = "<empty>".into();
-            }
+    //     //     
+    //     // })
+    //     // .for_each(|_| Ok(()));
  
-            bot.message(msg.chat.id, text).send()
-        })
-        .for_each(|_| Ok(()));
- 
-    bot.run_with(handle);
+    // bot.run_with(handle);
+    Ok(())
 }
 
 async fn get_latest_news() -> Result<String, reqwest::Error> {
-    let client = reqwest::Client::new();
-    let response = client.get("https://news.ycombinator.com/")
+    let http = reqwest::Proxy::http("127.0.0.1:20170").unwrap();
+    let client = reqwest::Client::builder().proxy(http).build()?;
+    
+    let response = client.get("https://hckrnews.com/")
         .send()
         .await?;
     let document = Document::from_read(response.bytes().await?.as_ref()).unwrap();
     let mut latest_news = String::new();
-    // Find the latest news
-    for node in document.find(Name("a")).take(30) {
+    let nodes = document.find(Class("story").and(Class("link"))).take(30);
+    let filtered_nodes = nodes.filter(|x| x.text() != "undefined \n");
+    for node in filtered_nodes {
         latest_news.push_str(&format!("{}\n", node.text()));
     }
 
